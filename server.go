470f7ff953c73f1f0aeacce9ec38ebe2729ca639
OscarClemente <oscar.clemente@gmail.com>
2021-05-01 14:46:24 +0200

    Added Chi router and cors allowed origin due to local test issue

--- server.go
+++ server.go
@@ -11,6 +11,8 @@ import (
 	"github.com/OscarClemente/backend-tech-challenge-time/db/postgres"
 	"github.com/OscarClemente/backend-tech-challenge-time/graph"
 	"github.com/OscarClemente/backend-tech-challenge-time/graph/generated"
+	"github.com/go-chi/chi"
+	"github.com/rs/cors"
 )
 
 const defaultPort = "8080"
@@ -25,11 +27,18 @@ func main() {
 		port = defaultPort
 	}
 
+	router := chi.NewRouter()
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:8080", "http://localhost:5000"},
+		AllowCredentials: true,
+		Debug:            false,
+	}).Handler)
+
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
